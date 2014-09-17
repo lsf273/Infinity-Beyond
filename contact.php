7f5bdffbc3779d72884409f0a8912a6c2a278a5f
Logan Fetter <loganfetter62@gmail.com>
2014-09-16 18:18:40 -0700

    Fixed the Form and Finished the PHP for Contact
    Closes #19

--- contact.php
+++ contact.php
@@ -2,30 +2,28 @@
   // echo phpinfo();
   $mysqli = mysqli_connect('localhost','lsf273_infinity','{tg#r5VT=8nU', 'lsf273_infinity-contact');
   if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-  }
-  echo $mysqli->host_info . "\n";
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  }	
   
   if($_SERVER['REQUEST_METHOD'] =='POST') {
 	 
-  $Name  = $_POST['name'];
-  $Email   = $_POST['email'];
-  $Subject   = $_POST['subject'];
-  $Message       = $_POST['message'];
+  $name  = $_POST['name'];
+  $email   = $_POST['email'];
+  $subject   = $_POST['subject'];
+  $message       = $_POST['message'];
   
-  $query = "INSERT INTO contact(Name, Email, Message)
-		   
-		   VALUES('$name','$email','$message')";
+  $query = "INSERT INTO contact(name, email, subject, message) VALUES('$name','$email', '$subject', '$message')";
 					
-					mysql_query($query) or die (mysql_error());
-  }
-  $to      = 'loganfetter62@gmail.com';
-  $subject = 'Message from Portfolio Website';
-  $msg     = "First Name: $first_name \n" . 
-			 "Last Name: $last_name \n" .
-			 "Email Address: $email \n" .
-			 "Subject: $subject \n" .
-			 "Message: $message \n";
-  mail($to, $subject, $msg, 'From:' . $email);
-  
+if ($mysqli->query($query) === TRUE) {
+		$to      = 'loganfetter62@gmail.com';
+		$subject = 'Message from Space Website';
+		$msg     = "Name: $name \n" . 
+		"Email Address: $email \n" .
+		"Subject: $subject \n" .
+		"Message: $message \n";
+		mail($to, $subject, $msg, 'From:' . $email);
+	} else {
+		die ($mysqli->error);
+	}
+}
 ?>
